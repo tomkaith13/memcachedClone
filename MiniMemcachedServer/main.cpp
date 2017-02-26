0c33b67c11fdd4dfa0067525448262718dbf8f2a
BIBIN THOMAS <bibinthomas@bibins-macbook-pro.local>
2017-02-25 17:16:52 -0800

    Initial Commit

---
+++ MiniMemcachedServer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MiniMemcachedServer
+//
+//  Created by BIBIN THOMAS on 2/25/17.
+//  Copyright © 2017 tomkaith13. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
